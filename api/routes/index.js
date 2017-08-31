a7e171825f64f59463e0b3949dbd8d75d0acb510
Pascal Knoth <pknoth@octo.com>
2017-08-31 10:50:54 +0200

    ping in scope /api/stats

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ const stats = require('../stats')
 const system = require('../system')
 
 exports.configure = function (app, options) {
-  app.use('/api', system(options))
+  app.use('/api/stats', system(options))
 
   app.use('/api/stats', stats(options))
 }
