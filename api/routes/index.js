96eece802ba80d23913fea7a3f5ed50eec238237
Raphaël Dubigny <rdubigny@gmail.com>
2019-07-05 19:14:35 +0200

    major refactor

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const stats = require('../stats')
-const system = require('../system')
-
-exports.configure = function (app, options) {
-  app.use('/api/stats', system(options))
-
-  app.use('/api/stats', stats(options))
-}
